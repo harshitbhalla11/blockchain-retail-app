ccbb3620f26c9d338f8d4f1cb4b6c67c566c15a6
harshit.bhalla.002 <harshit.bhalla.002@gmail.com>
2024-03-22 23:02:15 +0000

    update comp donate component and home comp

--- src/home/home.js
+++ src/home/home.js
@@ -1,10 +1,37 @@
 
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 
 const Home = () => {
   return (
     <div className="home-container">
       <div className="banner">
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="images/1.webp"
+              alt="First slide"
+            />
+          
+          </Carousel.Item>
+          <Carousel.Item>
+          <img
+              className="d-block w-100"
+              src="images/2.webp"
+              alt="First slide"
+            />
+         
+          </Carousel.Item>
+          <Carousel.Item>
+          <img
+              className="d-block w-100"
+              src="images/3.webp"
+              alt="First slide"
+            />
+         
+          </Carousel.Item>
+        </Carousel>
         <h1>Welcome to Our E-Commerce Store</h1>
         <p>Discover amazing products at great prices!</p>
       </div>
